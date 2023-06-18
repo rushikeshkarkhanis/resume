924ab19399047d535215cb4997c0d8ea9fb97150
virtuevedaintelligence <virtuevedaintelligence@gmail.com>
2023-06-18 12:36:44 +0530

    Resume like

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Getsocial from "../getsocial/Getsocial";
+import footercss from "../footer/footer.css"
 
 const Footer = () => {
   return (
     <>
-      <footer className="w3-container w3-blue-grey w3-center w3-margin-top" style={{ paddingBottom: "40px" }}>
+      <footer className="w3-container w3-blue-grey w3-center w3-margin-top footer-section" style={{ paddingBottom: "40px" }}>
         <Getsocial />
       </footer>
     </>
--- src/component/getsocial/Getsocial.js
+++ src/component/getsocial/Getsocial.js
@@ -4,23 +4,16 @@ const Getsocial = () => {
   return (
     <>
       <p>Be Social With Me.</p>
-      <a href="https://www.facebook.com/ashutosh.kolambkar" target="_blank">
+      <a href="https://www.facebook.com/rushi.karkhanis" target="_blank">
         <i className="footer-icons fa fa-facebook-official w3-hover-opacity"></i>
       </a>
-      <a href="https://www.instagram.com/k_o_l_i_g_r_a_p_h_y/" target="_blank">
+      <a href="https://www.instagram.com/karkhanisrushi/" target="_blank">
         <i className="footer-icons fa fa-instagram w3-hover-opacity"></i>
       </a>
-
-      <a href="https://kolambkar11.tumblr.com" target="_blank">
-        <i className="footer-icons fa fa-tumblr w3-hover-opacity"></i>
-      </a>
-      <a href="https://twitter.com/10Ashutosh10" target="_blank">
-        <i className="footer-icons fa fa-twitter w3-hover-opacity"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/ashutosh-kolambkar-23257a102/" target="_blank">
+      <a href="https://www.linkedin.com/in/rushikeshkarkhnais/" target="_blank">
         <i className="footer-icons fa fa-linkedin w3-hover-opacity"></i>
       </a>
-      <a href="https://wa.me/918692805802" target="_blank">
+      <a href="https://wa.me/919821792357" target="_blank">
         <i className="footer-icons fa fa-whatsapp w3-hover-opacity"></i>
       </a>
     </>
